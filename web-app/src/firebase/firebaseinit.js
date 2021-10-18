ef91831a220ded7440d3e776016af756afc21558
Mogakamo <mogaka.amo254@gmail.com>
2021-10-18 07:55:04 +0000

    refactor the firebaseinit file - it is updated to v9

--- web-app/src/firebase/firebaseinit.js
+++ web-app/src/firebase/firebaseinit.js
@@ -12,6 +12,7 @@ import { getAnalytics } from "firebase/analytics";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
+
 const firebaseConfig = {
   apiKey: "AIzaSyDugok9R0LeFFScBTFE8zJkmanxtYgTF2o",
 
