61af40d2afedaf6c799defc56ad470918e14adc9
Mogakamo <mogaka.amo254@gmail.com>
2021-10-20 12:35:58 +0000

    added the new firebase and configuring it

---
+++ web-app/src/firebase/firebaseinit.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseApp = initializeApp({
+  apiKey: "AIzaSyDugok9R0LeFFScBTFE8zJkmanxtYgTF2o",
+
+  authDomain: "invoice-app-e312b.firebaseapp.com",
+
+  projectId: "invoice-app-e312b",
+
+  storageBucket: "invoice-app-e312b.appspot.com",
+
+  messagingSenderId: "528654232475",
+
+  appId: "1:528654232475:web:597e7b767439392639cf3e",
+
+  measurementId: "G-75CLBCCNJY",
+});
